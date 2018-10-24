62a151295ab751b9a0bab4c95ecd35928f3bc994
Brian Wilson <Dynatrace-BrianWilson@users.noreply.github.com>
2018-10-24 11:25:32 -0600

    more changes

--- et_k8s/scripts/create.sh
+++ et_k8s/scripts/create.sh
@@ -5,6 +5,7 @@ kubectl create -f easytravel-backend-deployment.yaml
 kubectl create -f easytravel-backend-service.yaml
 kubectl create -f easytravel-frontend-deployment.yaml
 kubectl create -f easytravel-frontend-service.yaml
+kubectl create -f easytravel-frontend-service-2.yaml
 kubectl create -f easytravel-mongodb-deployment.yaml
 kubectl create -f easytravel-mongodb-service.yaml
-
+kubectl create -f easytravel-lb-deployment.yaml
--- et_k8s/scripts/delete.sh
+++ et_k8s/scripts/delete.sh
@@ -7,5 +7,7 @@ kubectl delete -f easytravel-frontend-deployment.yaml
 kubectl delete -f easytravel-frontend-service.yaml
 kubectl delete -f easytravel-mongodb-deployment.yaml
 kubectl delete -f easytravel-mongodb-service.yaml
+kubectl delete -f easytravel-frontend-service-2.yaml
+kubectl delete -f easytravel-lb-deployment.yaml
 
 
